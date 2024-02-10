c75195541cb9d623e5faa0a9f55692103b76401e
Swarup Sen <swarupsen080@gmail.com>
2024-02-11 03:21:43 +0530

    binding added in school home fragment

--- app/src/main/java/com/example/gmdf/fragment/SchoolHomeFragment.kt
+++ app/src/main/java/com/example/gmdf/fragment/SchoolHomeFragment.kt
@@ -6,14 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.gmdf.R
+import com.example.gmdf.databinding.FragmentSchoolHomeBinding
 
 class SchoolHomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentSchoolHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_school_home, container, false)
+        binding = FragmentSchoolHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
